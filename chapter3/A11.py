1ea834e45eb5c0771fdd756f4cfc0ca725ca4b46
yudai2929 <yudai201729@icloud.com>
2024-01-21 02:26:56 +0900

    👍A11.py

---
+++ chapter3/A11.py
@@ -0,0 +1,47 @@
+import sys
+
+
+def I():
+    return int(sys.stdin.readline().rstrip())
+
+
+def LI():
+    return list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def S():
+    return sys.stdin.readline().rstrip()
+
+
+def LS():
+    return list(sys.stdin.readline().rstrip().split())
+
+
+def main():
+    N, X = LI()
+    A = LI()
+
+    index = find_index_by_bin_search(A, X)
+
+    print(index + 1)
+
+
+def find_index_by_bin_search(nums: list[int], target: int) -> int:
+    left = 0
+    right = len(nums) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
+
+
+if __name__ == "__main__":
+    main()
