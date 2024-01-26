5f736956a9dede3fa4934ec48c495eafa0519f69
yudai2929 <yudai201729@icloud.com>
2024-01-26 23:49:36 +0900

    A19

---
+++ chapter4/A20.py
@@ -0,0 +1,25 @@
+import sys
+
+
+def I():
+    return int(sys.stdin.readline().rstrip())
+
+
+def LI():
+    return list(map(int, sys.stdin.readline().rstrip().split()))
+
+
+def S():
+    return sys.stdin.readline().rstrip()
+
+
+def LS():
+    return list(sys.stdin.readline().rstrip().split())
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
